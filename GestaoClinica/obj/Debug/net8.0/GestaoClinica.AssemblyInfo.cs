b54a08768521f96b3cd16500097e00199189384e
ericposser <eric_alegrete2002@hotmail.com>
2025-04-14 19:44:18 -0300

    commit .

--- GestaoClinica/obj/Debug/net8.0/GestaoClinica.AssemblyInfo.cs
+++ GestaoClinica/obj/Debug/net8.0/GestaoClinica.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestaoClinica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+547550d5cf4d976cde580c04e52f1d7d549a23bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestaoClinica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestaoClinica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
